19c2f05654669b32077d667a6c7f573f1d3b7a0f
samirbaracat <samirbaracat@gmail.com>
2025-01-12 10:18:20 -0400

    Inserindo novo projeto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -117,6 +117,14 @@ const Home = () => {
             <p className="font-normal text-gray-700 dark:text-gray-400">Monitoramento de uma API REST</p>
           </a>
 
+          <a href="https://github.com/samirbaracat/api-openfeign" target='_blank' className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div className="flex flex-wrap gap-3">
+              <BiLogoJava className='text-5xl rounded-full' />
+            </div>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Spring Cloud OpenFeign</h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Consumindo serviços em uma API RESTful</p>
+          </a>
+
         </div>
 
       </main>
