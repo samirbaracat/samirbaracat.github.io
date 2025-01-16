01c859a6203d1a24df70966c2c3aff06099abaad
samirbaracat <samirbaracat@gmail.com>
2025-01-15 20:47:27 -0400

    Incluindo projeto front-end

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -155,6 +155,16 @@ const Home = () => {
                     <p className="font-normal text-gray-700 dark:text-gray-400">Construindo uma Sidebar responsiva</p>
                   </a>
                 </li>
+                <li className='py-2'>
+                  <a href="https://github.com/samirbaracat/footer-responsive" target='_blank' className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <div className="flex flex-wrap gap-3">
+                      <FaReact className='text-5xl rounded-full' />
+                      <RiTailwindCssFill className='text-5xl rounded-full' />
+                    </div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">React e TailwindCSS</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Construindo um Footer responsivo</p>
+                  </a>
+                </li>
               </ul>
             </div>
 
